7f7de2c3e269ab18954897a51aa0b08491b8ce7a
sai04032006 <pulimilokesh11@gmail.com>
2024-11-01 16:19:02 +0530

    HELLO

---
+++ studentapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+app_name = 'studentapp'
+
+urlpatterns = [
+    path('StudentHomePage/', views.StudentHomePage, name='StudentHomePage'),
+    path('feedback/', views.feedback, name='feedback'),
+    path('feedback/success/', views.feedback_success, name='feedback_success'),
+
+]
